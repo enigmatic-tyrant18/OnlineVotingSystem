d06021d5d18bc0a4c615e1f5bc062cd91d7df03c
AYUSH BHAGAT <96322425+enigmatic-tyrant18@users.noreply.github.com>
2023-11-25 20:34:33 +0530

    onlinevotingsystem

---
+++ admin/inc/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'localhost';
+$user = 'bhagatayush710@gmail.com';
+$password = 'bhagatji';
+$database = 'onlinevotingsystem';
+
+$db = mysqli_connect('localhost', 'bhagatayush710@gmail.com', 'bhagatji', 'onlinevotingsystem') or die("Conectivity Failed");
+
+?>
